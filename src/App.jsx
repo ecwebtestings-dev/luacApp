54d1723c8cbc93d390f72a442fdc98191f7ab926
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-08-27 11:28:11 +0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/LandingPage/Home'
 import Signup from './pages/Auth/RegisterPage'
 import Login from './pages/Auth/LoginPage'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
