5d0933c41d85069dae6b7cd873264416817de7e5
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-08-27 11:29:50 +0300

    Login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,14 @@ import Activities from "./Activities";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import WhyJoin from "./JoinUs";
+import Nav from "./NavigationBar";
 import TopBar from "./TopBar";
 
 export default function Home() {
   return (
     <div className="bg-pagebg font-Inter">
       <TopBar/>
-      
+      <Nav/>
       <Hero/>
       <About/>
       <Activities/>
