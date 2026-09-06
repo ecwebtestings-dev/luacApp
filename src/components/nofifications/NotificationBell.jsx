227a0aeb2649e8a43703d0cf6d77808df3420b78
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-09-06 11:06:15 +0300

    Dashboard Updates

--- src/components/nofifications/NotificationBell.jsx
+++ src/components/nofifications/NotificationBell.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useNotifications, useMarkNotificationsRead } from '../../hooks/useNotifications';
 import {
   iconForNotification, messageOf, isUnread, timeAgo, extractList, extractUnreadCount,
-} from '../../utils/notificationUtils';
+} from '../../utils/NotificationUtils';
 import Spinner from '../common/Spinner';
 
 export default function NotificationBell({ notificationsPath = '/dashboard/notifications' }) {
--- src/components/nofifications/notificationRow.jsx
+++ src/components/nofifications/notificationRow.jsx
@@ -1,5 +1,5 @@
 import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
-import { iconForNotification, messageOf, isUnread, timeAgo } from '../../utils/notificationUtils';
+import { iconForNotification, messageOf, isUnread, timeAgo } from '../../utils/NotificationUtils';
 
 export default function NotificationRow({ notification, onOpenActions }) {
   const Icon = iconForNotification(notification);
