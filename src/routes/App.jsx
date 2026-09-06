8389699d7bbd7f5ed3e5bd33e262491e8464cc89
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-09-06 10:48:59 +0300

    Dashboard Updates

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -10,7 +10,7 @@ import Home from '../pages/Home/Home'
 
 //LAYOUT
 import ProtectedRoute from './protectedRoutes'
-import DashboardLayout from '../components/layout/DashboardLayout'
+import DashboardLayout from '../components/layout/dashboardLayout'
 
 //STUDENT USER PAGES
 
